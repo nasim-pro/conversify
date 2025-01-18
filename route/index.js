5188ed2d88eaad77d55e39ac276751b967323d3f
Nasim Ahmad <khannasimahmad@gmail.com>
2025-01-18 17:21:22 +0530

    removed group and groupmember related route from index

--- route/index.js
+++ route/index.js
@@ -1,11 +1,9 @@
 const { Router } = require('express');
 const allRoute = Router();
 const userRoute = require('./user.route');
-const groupRoute = require('./group.route');
 const messageRoute = require('./message.route');
 
 allRoute.use('/user', userRoute);
-allRoute.use('/group', groupRoute);
 allRoute.use('/message', messageRoute);
 
 module.exports = allRoute;
