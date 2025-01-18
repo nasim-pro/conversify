7131b2baf3f862d3ba0d195270712e8ecae02f0b
Nasim Ahmad <khannasimahmad@gmail.com>
2025-01-18 17:46:55 +0530

    removed everything

--- route/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const allRoute = Router();
-const userRoute = require('./user.route');
-const messageRoute = require('./message.route');
-
-allRoute.use('/user', userRoute);
-allRoute.use('/message', messageRoute);
-
-module.exports = allRoute;
