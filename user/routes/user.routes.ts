647977eeb1bb3d0a0151adf147dc3c8e981fbef4
Nasim Ahmad <khannasimahmad@gmail.com>
2025-01-23 22:36:30 +0530

    removed user from the user routes kept only forward slashes

--- user/routes/user.routes.ts
+++ user/routes/user.routes.ts
@@ -3,7 +3,7 @@ export const userRouter = Router()
 import { create, userList, login } from '../controller/user.controller.ts'
 import { isAuthorize } from "../../middleware/verifyjwt.ts";
 
-userRouter.post('/user', create);
-userRouter.get('/user', isAuthorize,  userList );
-userRouter.post('/user/login', login);
+userRouter.post('/', create);
+userRouter.get('/', isAuthorize,  userList );
+userRouter.post('/login', login);
 
